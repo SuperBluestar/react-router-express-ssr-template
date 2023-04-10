d713a3ffe0c1c4d63e6e744c3f06e462630a7cb6
SuperBluestar <81801796+SuperBluestar@users.noreply.github.com>
2023-04-11 07:03:02 +0900

    Remove unnecessary codes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import AboutPage from './pages/About';
 
 function Router() {
   return (
-    // <HomePage {...props} />
     <Routes>
       <Route path='' element={<Homepage />} />
       <Route path='about' element={<AboutPage />} />
